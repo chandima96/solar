c89890d0a3407642ea92c8a9fa1131c71e207222
dalla1996 <asankatharindu1111@gmail.com>
2023-11-26 09:54:02 +0530

    tailwind install

--- ims/tailwind.config.js
+++ ims/tailwind.config.js
@@ -6,6 +6,10 @@ module.exports = {
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+            "./resources/**/*.blade.php",
+            "./resources/**/*.js",
+            "./resources/**/*.vue",
+          
     ],
 
     theme: {
