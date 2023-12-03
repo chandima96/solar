9c953963e7f62575c3c534252e81a6424f4f5e55
Nikalanda Bandara <chandima96dilan@gmail.com>
2023-12-03 12:02:01 +0530

    Test

--- ims/routes/web.php
+++ ims/routes/web.php
@@ -21,6 +21,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/inventory', function () {
+    return view('inventory');
+});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
