9289db262a7c09bf783b4983084eeae18d5cdd76
Nikalanda Bandara <chandima96dilan@gmail.com>
2023-12-06 08:50:48 +0530

    Test

--- ims/resources/views/auth/login.blade.php
+++ ims/resources/views/auth/login.blade.php
@@ -1,5 +1,5 @@
 <x-guest-layout>
-    <!-- Session Status -->
+    <!-- Session Status test -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
     <form method="POST" action="{{ route('login') }}">
