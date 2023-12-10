89991d41c48146e89b97995471671a26874f238b
Nikalanda Bandara <chandima96dilan@gmail.com>
2023-12-10 16:58:31 +0530

    Test

--- ims/resources/views/category.blade.php
+++ ims/resources/views/category.blade.php
@@ -133,8 +133,11 @@
             </div>
             <!-- model Update -->
 
+
+
+        </div>
             <!-- model add  -->
-            <div id="addModal" class="fixed inset-0 z-10 overflow-y-auto hidden">
+              <div id="addModal" class="fixed inset-0 z-10 overflow-y-auto hidden">
               <div class="flex items-center justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
                 <div class="fixed inset-0 transition-opacity" aria-hidden="true">
                   <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
@@ -184,8 +187,6 @@
               </div>
             </div>
             <!-- model add -->
-
-        </div>
 
         <!-- success message -->
         <div id="successMessage" class="fixed top-0 right-0 mt-12 mr-10 bg-white text-sm text-green-600 p-1 rounded shadow-md transform translate-x-full opacity-0 transition-transform duration-500 ease-in-out">
