4982e91a5ab5ebdb2e24621f4e8d749850900c68
Asanka Tharindu <51691013+dalla1996@users.noreply.github.com>
2024-01-03 22:35:14 +0530

    Update welcome.blade.php

--- ims/resources/views/welcome.blade.php
+++ ims/resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
 
-        <title>Laravel</title>
+        <title>Laravel New</title>
 
         <!-- Fonts -->
         <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
